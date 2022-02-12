fd1faf1ccca56a11c0570d64700875af6dcc4138
Misqke <misqke.rust@yahoo.com>
2022-02-12 08:34:11 -0500

    animate portfolio page

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import styles from "../styles/portfolio.module.scss";
 
@@ -21,12 +21,32 @@ const portfolio = () => {
     "webbrain.jpg",
   ];
 
+  useEffect(() => {
+    const imgs = document.querySelectorAll(`#portfolio_wrapper > div`);
+    console.log(imgs);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          entry.target.classList.toggle(
+            `${styles.visible}`,
+            entry.isIntersecting
+          );
+        });
+      },
+      { threshold: 0.35 }
+    );
+
+    imgs.forEach((img) => {
+      observer.observe(img);
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>PORTFOLIO</h1>
-      <div className={styles.wrapper}>
+      <div className={styles.wrapper} id="portfolio_wrapper">
         {images.map((img, i) => (
-          <div className={styles.img_container} key={i}>
+          <div className={`${styles.img_container}`} key={i}>
             <Image
               src={`/${img}`}
               width={400}
